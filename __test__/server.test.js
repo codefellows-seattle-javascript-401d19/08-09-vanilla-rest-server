2f4d712f4b72ccac9fc678c31e04fae44172a91b
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-06 22:41:29 -0800

    passing all tests for POST and GET

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,7 +2,7 @@
 
 const server = require('../lib/server');
 const superagent = require('superagent');
-let heroIndex;
+// let heroIndex;
 const PORT = process.env.PORT;
 
 describe('/api/heroes', () => {
@@ -39,14 +39,24 @@ describe('/api/heroes', () => {
       });
   });
 
-  // test('GET should contain a response body for a request made with a valid id', () => {
-  //   return superagent.get(`http://localhost:${PORT}/api/heroes?id=${heroIndex}`)
-  //     .then(response => {
-  //       expect(response.status).toEqual(200);
-  //       // expect(response.body.name).toEqual('Kain');
-  //       // expect(response.body.superPower).toEqual('Immortality');
-  //     });
-  // });
+  test('GET should contain a response body for a request made with a valid id', () => {
+    return superagent.post('http://localhost:3000/api/heroes')
+      .set('Content-Type', 'application/json')
+      .send({
+        name : 'Kain',
+        superPower : 'Immortality',
+      })
+      .then(response => {
+        return superagent.get(`http://localhost:${PORT}/api/heroes`)
+          .query({id: response.body.id});
+      })
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.name).toEqual('Kain');
+        expect(response.body.superPower).toEqual('Immortality');
+      });
+  });
+
 
   test('GET should respond with 404 status code if no request body was provided or the body was invalid', () => {
     return superagent(`http://localhost:${PORT}/api/heroes?id=testing`)
