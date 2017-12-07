502fb572cf5e664a252932104687415ef78f9663
David Lindahl <davidalindahl@gmail.com>
2017-12-07 15:36:39 -0800

    wrote more tests per assignment

--- lab-david/__test__/server.test.js
+++ lab-david/__test__/server.test.js
@@ -7,7 +7,49 @@ describe('api/mountains',() => {
   beforeAll(server.start);
   afterAll(server.stop);
 
-  test(`should respond with a 200 status code and a valid body`, () => {
+  test(`should respond with a 404, not found, for valid requests with an id that was not found`, () => {
+    return superagent.get(`http://localhost:3000/api/mountains`)
+      .set('Content-Type','application/json')
+      .send({
+        id : 'null',
+      })
+      .then(response => {
+        expect(response.status).toEqual(404);
+      });
+  });
+
+  test(`should respond with a 200 status code for a request with a valid id`, () => {
+    return superagent.get(`http://localhost:3000/api/mountains`)
+      .set('Content-Type','application/json')
+      .send({
+        name : 'Shuksan',
+        state : 'Washington',
+        hiking : 'Yes',
+        range : 'Cascades',
+      })
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.id).toBeTruthy();
+        
+        expect(response.body.name).toEqual('Shuksan');
+        expect(response.body.state).toEqual('Washington');
+        expect(response.body.hiking).toEqual('Yes');
+        expect(response.body.range).toEqual('Cascades');
+      });
+  });
+
+  test(`should respond with a 400 bad request error if no request body was provided or body was invalid`, () => {
+    return superagent.post(`http://localhost:3000/api/mountains`)
+      .set('Content-Type','application/json')
+      .send({
+        fakeName : 'fakeShuksan',
+      })
+      .then(response => {
+        expect(response.status).toEqual(400);
+      });
+  });
+
+  test(`should respond with a 200 status code for a post request with a valid body`, () => {
     return superagent.post(`http://localhost:3000/api/mountains`)
       .set('Content-Type','application/json')
       .send({
@@ -26,21 +68,5 @@ describe('api/mountains',() => {
         expect(response.body.range).toEqual('Cascades');
       });
   });
-  //TODO : write more tests based on the assignment
-  test(`should respond with a 400 bad request error if no request body was provided or body was invalid`, () => {
-    return superagent.post(`http://localhost:3000/api/mountains`)
-      .set('Content-Type','application/json')
-      .send({
-        fakeName : 'fakeShuksan',
-      })
-      .then(response => {
-        expect(response.status).toEqual(400);
-        // expect(response.body.id).toBeTruthy();
-        
-        // expect(response.body.name).toEqual('Shuksan');
-        // expect(response.body.state).toEqual('Washington');
-        // expect(response.body.hiking).toEqual('Yes');
-        // expect(response.body.range).toEqual('Cascades');
-      });
-  });
+  
 });
