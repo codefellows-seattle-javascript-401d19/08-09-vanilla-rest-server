63d6e4a08c37aac168edc9e6f2908e1da4f31d70
David Lindahl <davidalindahl@gmail.com>
2017-12-06 16:31:29 -0800

    built out server file

--- lab-david/lib/server.js
+++ lab-david/lib/server.js
@@ -6,4 +6,51 @@ const router = require('./router');
 
 process.env.PORT = 3000;
 
-require('../route/note-router');
+require('../route/mountain-router');
+
+const app = http.createServer(router.route);
+
+let isServerOn = false;
+
+const server = module.exports = {};
+
+server.start = () => {
+  return new Promise((resolve, reject) => {
+    if(isServerOn){
+      logger.log(`error`, `---->SERVER_ERROR<---- server is already running`);
+      return reject(new Error(`---->SERVER_ERROR<---- server is already running`));
+    }
+    if(!process.env.PORT){
+      logger.log(`error`, `---->SERVER_ERROR<---- PORT variable is not configured - get on it`);
+      return reject(new Error(`---->SERVER_ERROR<---- PORT variable is not configured`));
+    }
+    app.listen(process.env.PORT, error => {
+      if(error)
+        return reject(error);
+
+      isServerOn = true;
+      logger.log(`info`, `Server is online on port ${process.env.PORT}`);
+      console.log(`info`, `server is online on port ${process.env.PORT}`);
+      return resolve();
+    });
+  });
+};
+
+server.stop = () => {
+  return new Promise((resolve,reject) => {
+    if(!ServerOne){
+      logger.log('error', `---->SERVER_ERROR<---- server is already off dude. go chill`);
+      return reject(new Error(`---->SERVER_ERROR<---- server is already off dude. go chill`));
+    }
+    app.close(error => {
+      if(error){
+        logger.log(`error`, `---->SERVER_ERROR<---- server can't be shut down. just like skynet`);
+        logger.log(`error`, error);
+
+        return reject(error);
+      }
+      isServerOn = false;
+      return resolve();
+    });
+  });
+};
