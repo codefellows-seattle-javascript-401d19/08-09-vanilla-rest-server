99175f9dfe04e3f7caeab76c3aacecdbdb062510
Robert Reed <robert.mc.reed@gmail.com>
2017-12-07 15:33:27 -0800

    got rid of hard coded ports in test

--- lab-rob/__test__/server.test.js
+++ lab-rob/__test__/server.test.js
@@ -2,6 +2,9 @@
 
 const server = require('../lib/server');
 const superagent = require('superagent');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 
 describe('/api/trials-bikes', () => {
   beforeAll(server.start);
@@ -17,7 +20,7 @@ describe('/api/trials-bikes', () => {
   };
 
   test('POST should respond with a 200 status code and a body if there are no errors.', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .send(scorpa)
       .then(res => {
@@ -35,7 +38,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('POST should respond with a 400 status code if no/invalid body, and an object with an error property.', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .catch(res => {
         expect(res.status).toEqual(400);
@@ -44,7 +47,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('POST should respond with a 400 status code if body has no make property, and an object with an error property.', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .send({})
       .catch(res => {
@@ -54,7 +57,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('POST should respond with a 400 status code if body has no/invalid model property, and an object with an error property.', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .send({make: '', model: undefined})
       .catch(res => {
@@ -64,7 +67,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('POST should respond with a 400 status code if body has no/invalid year property, and an object with an error property.', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .send({make: '', model: '', year: true})
       .catch(res => {
@@ -74,7 +77,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('POST should respond with a 400 status code if body has no/invalid displacement property, and an error object with error an property.', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .send({ make: '', model: '', year: 3, displacement: null })
       .catch(res => {
@@ -84,7 +87,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('POST should respond with a 400 status code if body has no/invalid color property, and an object with an error property."', () => {
-    return superagent.post('http://localhost:3000/api/trials-bikes')
+    return superagent.post(`http://localhost:${PORT}/api/trials-bikes`)
       .set('Content-Type', 'application/json')
       .send({make: '', model: '', year: 3, displacement: 4, color: 9})
       .catch(res => {
@@ -94,7 +97,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('GET should respond with a 200 status code and an array of all objects if no id is given.', () => {
-    return superagent.get('http://localhost:3000/api/trials-bikes')
+    return superagent.get(`http://localhost:${PORT}/api/trials-bikes`)
       .then(res => {
         expect(res.status).toEqual(200);
         expect(res.body).toEqual(testArray);
@@ -102,7 +105,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('GET should respond with a 200 status code and a single object when id is given.', () => {
-    return superagent.get(`http://localhost:3000/api/trials-bikes?id=${testId}`)
+    return superagent.get(`http://localhost:${PORT}/api/trials-bikes?id=${testId}`)
       .then(res => {
         expect(res.status).toEqual(200);
         expect(res.body).toEqual(testArray[0]);
@@ -110,7 +113,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('GET should respond with a 404 status code and an object with error property "No bike with id "<id>".', () => {
-    return superagent.get(`http://localhost:3000/api/trials-bikes?id=hamburger`)
+    return superagent.get(`http://localhost:${PORT}/api/trials-bikes?id=hamburger`)
       .catch(res => {
         expect(res.status).toEqual(404);
         expect(JSON.parse(res.response.res.text).error).toEqual('No bike with id "hamburger".');
@@ -118,7 +121,7 @@ describe('/api/trials-bikes', () => {
   });
 
   test('DELETE should respond with a 204 status code and an empty body if the id is valid.', () => {
-    return superagent.delete(`http://localhost:3000/api/trials-bikes?id=${testId}`)
+    return superagent.delete(`http://localhost:${PORT}/api/trials-bikes?id=${testId}`)
       .then(res => {
         expect(res.status).toEqual(204);
         expect(res.body).toEqual({});
@@ -126,7 +129,7 @@ describe('/api/trials-bikes', () => {
   });
   
   test('DELETE should respond with a 400 status code if no id is given.', () => {
-    return superagent.delete(`http://localhost:3000/api/trials-bikes`)
+    return superagent.delete(`http://localhost:${PORT}/api/trials-bikes`)
       .catch(res => {
         expect(res.status).toEqual(400);
         expect(JSON.parse(res.response.res.text).error).toEqual('bad request, no id.');
@@ -134,7 +137,7 @@ describe('/api/trials-bikes', () => {
   });
   
   test('DELETE should respond with a 404 status code if no bike with the given id is on the server.', () => {
-    return superagent.delete(`http://localhost:3000/api/trials-bikes?id=hotdogs`)
+    return superagent.delete(`http://localhost:${PORT}/api/trials-bikes?id=hotdogs`)
       .catch(res => {
         expect(res.status).toEqual(404);
         expect(JSON.parse(res.response.res.text).error).toEqual('No bike with id "hotdogs".');
