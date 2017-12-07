1c1a2460ca0686b7d5e2b7dcf92f6c1193621d4c
David Lindahl <davidalindahl@gmail.com>
2017-12-07 10:21:08 -0800

    adding type check to mountain inputs

--- lab-david/model/mountain.js
+++ lab-david/model/mountain.js
@@ -4,8 +4,16 @@ const uuid = require('uuid/v1');
 
 class Mountain{
   constructor(name,state,hiking,range){
+    if(typeof name !== 'string')
+      throw TypeError('ERROR - name must be a string');
+    if(typeof state !== 'string')
+      throw TypeError('ERROR - state must be a string');
+    if(typeof hiking !== 'string')
+      throw TypeError('ERROR - hiking must be a string');
+    if(typeof range !== 'string')
+      throw TypeError('ERROR - range must be a string');
+    
     this.id = uuid();
-
     this.name = name;
     this.state = state;
     this.hiking = hiking;
