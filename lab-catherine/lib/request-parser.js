5efb5b6c12a8ea455fc7820c7de2bf0af27d3f65
Catherine Looper <loopercat@gmail.com>
2017-12-06 16:31:02 -0800

    Added an index.js file to initialize the server with npm start

--- lab-catherine/lib/request-parser.js
+++ lab-catherine/lib/request-parser.js
@@ -6,30 +6,21 @@ const logger = require('./logger');
 const requestParser = module.exports = {};
 
 requestParser.parse = (request) => {
-  return new Promise((resolve,reject) => {
-    //-------------------------------------------------------------
-    // This will be done for ALL requests
-    //-------------------------------------------------------------
+  return new Promise((resolve, reject) => {
     logger.log('debug', `Original URL: ${JSON.stringify(request.url)}`);
-    request.url = urlModule.parse(request.url,true);
+    request.url = urlModule.parse(request.url, true);
     logger.log('debug', `Parsed URL: ${JSON.stringify(request.url)}`);
 
     if(request.method !== 'POST' && request.method !== 'PUT')
       return resolve(request);
-    //-------------------------------------------------------------
-    // Parsing a body is JUST for HTTP methods that include a body
-    //-------------------------------------------------------------
-    //  if I'm in this part of the code, I know it's a POST/PUT request
+ 
     let sentText = '';
-    request.on('data',(buffer) => {
+    request.on('data', (buffer) => {
       sentText += buffer.toString();
     });
 
-    request.on('end',() => {
-      try{
-        //  this is mutating the request object, and creating an
-        //           body property
-        //           Here, we were ASSUMING that  sentText is JSON
+    request.on('end', () => {
+      try {
         if(request.headers['content-type'].indexOf('application/json') > -1){
           request.body = JSON.parse(sentText);
           return resolve(request);
