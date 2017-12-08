3f52180ab7bcd309de81b66cebaecb7314eb80f5
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-07 16:54:37 -0800

    fix bug testing POST requests; trying to figure out bug testing GET requests

--- route/sweet-router.js
+++ route/sweet-router.js
@@ -7,15 +7,16 @@ const logger = require(`../lib/logger`);
 let sweets = [];
 
 let sendStatus = (response, status, message) => {
+  logger.log(`info`, `ERROR: ${message}`);
   logger.log(`info`, `Sending status: ${status}`);
 
-  response.writeHead(status);   //only status code is required for writeHead()
+  response.writeHead(status);   //only Status Code is required for writeHead()
   response.end();
   return;
 };
 
 let sendJSON = (response, status, jsonData) => {
-  logger.log(`info`, `Sending JSON! ${jsonData}`);
+  logger.log(`info`, `Sending JSON!`);
 
   response.writeHead(status, {
     'Content-Type' : 'application/json'
@@ -27,25 +28,39 @@ let sendJSON = (response, status, jsonData) => {
 
 router.post(`/api/sweets`, (request, response) => {
   if(!request.body.name){
-    return sendStatus(response, 400, 'A sweet has no name');
+    return sendStatus(response, 404, 'A sweet has no name');
   }
 
   if(!request.body.hasChocolate){
-    return sendStatus(response, 400, 'Does the sweet have chocolate? Yes or no, man!');
+    return sendStatus(response, 404, 'Does the sweet have chocolate? Yes or no, man!');
   }
 
   if(!request.body.temperature){
-    return sendStatus(response, 400, 'Could not find the temperature of the sweet :(');
+    return sendStatus(response, 404, 'Could not find the temperature of the sweet :(');
   }
 
-  sweet = new Sweet(request.body.name, request.body.hasChocolate, request.body.temperature);
+  let sweet = new Sweet(request.body.name, request.body.hasChocolate, request.body.temperature);
   sweets.push(sweet);
-
   sendJSON(response, 200, sweet);
+  logger.log(`info`, `The new sweet's id is :${sweet.id}`);
 });
 
+
 router.get(`/api/sweets`, (request, response) => {
-  
+  let id = request.url.query.id;
+  console.log(request.url.query.id, `is the id`);
+  console.log(sweets, `are the sweets`);
+  // let matchingSweet = the object in the sweets that has an id property with a value that matched 'id';
+  let matchingSweet = sweets.filter(sweet => sweet[id] === id);
+  console.log(matchingSweet, `is the matching sweet`);
+
+  if(request.url.query.id){
+    //if none of the objects in sweets array has the id, return an error
+    if(!matchingSweet){
+      return sendStatus(response, 404, `There is no sweet with that id`);
+    }
+    return sendJSON(response, 200, matchingSweet);  //if there is a match, give me that
+  }
 
   sendJSON(response, 200, sweets);
 });
