de86c9249be0b3be4874e6bb72a480431edfb616
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-07 19:40:05 -0800

    deleting hero from the storage

--- route/hero-router.js
+++ route/hero-router.js
@@ -51,84 +51,39 @@ router.post('/api/heroes', (request, response) => {
     });
 });
 
-//-------- GET REQUEST ----------------
-
-// ========= OLD WAY =============
+//-------- GET REQUEST with storage -------------
 
 router.get('/api/heroes', (request, response) => {
   if(request.url.query.id) {
-    let heroName;
-    heroes.forEach((hero) => {
-      if(request.url.query.id === hero['id']) {
-        heroName = hero;
-        return;
-      }
-    });
-
-    if(!heroName) {
-      sendStatus(response, 404, '__HERO_ID_NOT_FOUND__');
-      return;
-    }
-    sendJSON(response, 200, heroName);
-  } else {
-    sendJSON(response, 200, heroes);
-  }
-});
-
-// ========= NEW WAY ============
-
-router.get('/api/heroes', (request, response) => {
-
-  // return storage.fetchI
-
-
-  if(request.url.query.id) {
-    let heroName;
-    heroes.forEach((hero) => {
-      if(request.url.query.id === hero['id']) {
-        heroName = hero;
-        return;
-      }
-    });
-
-    if(!heroName) {
-      sendStatus(response, 404, '__HERO_ID_NOT_FOUND__');
-      return;
-    }
-    sendJSON(response, 200, heroName);
-  } else {
-    storage.fetchAll()
+    storage.fetchItem(request.url.query.id)
       .then((result) => {
         sendJSON(response, 200, result);
       })
       .catch(error => {
         sendStatus(response, 404, error);
       });
+
+  } else {
+    storage.fetchAll()
+      .then((result) => {
+        sendJSON(response, 200, result);
+      })
+      .catch(error => {
+        sendStatus(response, 500, error);
+      });
   }
 });
 
-// -------- DELETE REQUEST ----------------
+//-------- DELETE REQUEST with storage -------------
 
 router.delete('/api/heroes', (request, response) => {
   if(!request.url.query.id) {
-    sendStatus(response, 400, 'lack of ID in query');
+    sendStatus(response, 400, '__DELETE_ERROR__ no ID in query');
     return;
   }
-  let heroName;
-  heroes.forEach((hero) => {
-    if(request.url.query.id === hero['id']) {
-      heroName = hero;
-      return;
-    }
-  });
-  let heroIndex = heroes.indexOf(heroName);
-  if(!heroName) {
-    sendStatus(response, 404, 'This hero doesn\'t exist, hero ID not found');
-    return;
-  } else {
-    console.log(`inside DELETE with ${heroName}`);
-    heroes.splice(heroIndex, 1);
-    console.log(`This is my new array of heroes: ${heroes}`);
-    sendStatus(response, 204, 'SUCCESSFULLY__DELETING__A__HERO');
-  }
+
+  storage.deleteItem(request.url.query.id)
+    .then((result) => {
+      sendJSON(response, 200, result);
+    });
 });
